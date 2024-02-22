4da67d542caae96abc97d2d74b20ab475edf7995
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-02-22 21:25:47 +0530

    importing json file

---
+++ src/db/data.js
@@ -0,0 +1,63 @@
+
+export const data = [
+    {
+      img: "https://res.cloudinary.com/djdzjr3a5/image/upload/v1706389895/1_zp5mym.png",
+      title: "Future Of Play: Building The Next-Gen of Gaming",
+      companyName: "Elevation Capital",
+      category: "consumer",
+    },
+    {
+      img: "https://res.cloudinary.com/djdzjr3a5/image/upload/v1706389873/2_avfqoc.png",
+      title: "Mapping A Billion: Understanding The Indian Fintech User",
+      companyName: "Elevation Capital",
+      category: "finance",
+    },
+    {
+      img: "https://res.cloudinary.com/djdzjr3a5/image/upload/v1706389874/7_quolzd.png",
+      title: "Battery Bluprint Innovation & Investment Outlook",
+      companyName: "Blume Ventures",
+      category: "renewable",
+    },
+    {
+      img: "https://res.cloudinary.com/djdzjr3a5/image/upload/v1706389874/9_cronpk.png",
+      title: "Indian Edtech In 2022: Building In The Shadow of Giantsg",
+      companyName: "Blume Ventures",
+      category: "consumer",
+    },
+    {
+      img: "https://res.cloudinary.com/djdzjr3a5/image/upload/v1706389918/19_z9qqx7.png",
+      title: "Agritech: India’s Sunrise Sector",
+      companyName: "Kaalari Capital",
+      category: "agritech",
+    },
+    {
+      img: "https://res.cloudinary.com/djdzjr3a5/image/upload/v1706389878/22_hxni5z.png",
+      title: "Beyond E-Commerce 3.0 : D2C, Ecom Enablement & ONDC",
+      companyName: "Kaalari Capital",
+      category: "consumer",
+    },
+    {
+      img: "https://res.cloudinary.com/djdzjr3a5/image/upload/v1708266967/81_ihw0nk.png",
+      title: "Agritech: Leading next decade‘s tech-first value creation",
+      companyName: "Avendus Capital",
+      category: "agritech",
+    },
+    {
+      img: "https://res.cloudinary.com/djdzjr3a5/image/upload/v1706389904/56_ewfitm.png",
+      title: "Entertainment & Advertising : Riding The Digital Wave",
+      companyName: "RedSeer",
+      category: "social",
+    },
+    {
+      img: "https://res.cloudinary.com/djdzjr3a5/image/upload/v1706389928/52_inba1h.png",
+      title: "Logistics Needs of Indian D2C",
+      companyName: "RedSeer",
+      category: "consumer",
+    },
+    {
+      img: "https://res.cloudinary.com/djdzjr3a5/image/upload/v1706389913/55_xuarij.png",
+      title: "Edtech In India 2019-20",
+      companyName: "RedSeer",
+      category: "consumer",
+    },
+  ];
