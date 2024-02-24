db1295a3363135a61831e5270042d50983c371fd
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-02-24 16:09:20 +0530

    changes in ui of contact page

--- src/components/contactpage/Contact.jsx
+++ src/components/contactpage/Contact.jsx
@@ -1,6 +1,32 @@
 import "./Contact.css";
-
+import React, { useState } from "react";
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // You can handle form submission logic here, like sending data to a server
+    console.log(formData);
+    // Clear form fields after submission
+    setFormData({
+      name: "",
+      email: "",
+      message: "",
+    });
+  };
+
   return (
     <div className="container">
       <h3 className="h3">Connect with us</h3>
@@ -49,7 +75,67 @@ const Contact = () => {
         </p>
       </div>
       <div className="container-right">
-        
+        <form className="form" onSubmit={handleSubmit}>
+          <div className="name">
+            <label className="namechange" htmlFor="name">
+              Name
+            </label>
+            <input
+              className="inputname"
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <div className="email">
+            <label className="mail" htmlFor="email">
+              Email
+            </label>
+            <input
+              className="inputmail"
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="checklist">
+          <div >
+            <input type="checkbox" id="checklist" name="checklist" />
+            <label htmlFor="checklist">Checklist Item 1</label>
+          </div>
+
+          <div>
+            <input type="checkbox" id="checklist2" name="checklist2" />
+            <label htmlFor="checklist2">Checklist Item 2</label>
+          </div>
+          </div>
+          
+
+          <label className="messg" htmlFor="message">
+            Message
+          </label>
+          <div className="textarea">
+            {" "}
+            <textarea
+              id="message"
+              name="message"
+              placeholder="Write your message here."
+              value={formData.message}
+              onChange={handleChange}
+              rows="4"
+              required
+            ></textarea>
+          </div>
+
+          <button type="submit">Submit</button>
+        </form>
       </div>
     </div>
   );
