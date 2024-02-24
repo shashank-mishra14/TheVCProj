dd633f7e1f374ff355941d1ad432cfef8a092c54
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-02-24 15:28:54 +0530

    changes

--- src/components/contactpage/Contact.jsx
+++ src/components/contactpage/Contact.jsx
@@ -1,9 +1,26 @@
+import "./Contact.css";
+
 const Contact = () => {
   return (
     <div className="container">
-      <div className="container-left"> </div>
-      <div className="container-right"> </div>
+      <h3 className="h3">Connect with us</h3>
+
+      <h1 className="h1">Submit your report</h1>
+      <div className="container-left">
+        <p className="paragraph">
+          "One of the standout features of the VC Project Repository is its
+          user-friendly interface. Navigating through the platform was
+          intuitive, allowing our team to effortlessly browse through a diverse
+          range of research topics. The search functionality was robust,
+          enabling us to quickly locate specific studies or explore broader
+          themes within the VC domain. Additionally, the categorization of
+          researches based on relevant tags enhanced the overall user
+          experience, ensuring that users could easily find content tailored to
+          their interests."
+        </p>
+      </div>
+      <div className="container-right"></div>
     </div>
   );
-};  
+};
 export default Contact;
