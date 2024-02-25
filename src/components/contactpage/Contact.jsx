b4ad5a14346c9e46e0db8a1e45095c8f3e84cfbf
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-02-25 15:33:03 +0530

    changed the checklist to an oject

--- src/components/contactpage/Contact.jsx
+++ src/components/contactpage/Contact.jsx
@@ -8,6 +8,26 @@ const Contact = () => {
     message: "",
   });
 
+  const checklistData = [
+    {
+      checklistQuery: "Problem in uploading research?",
+    },
+    {
+      checklistQuery: "Unable to find the research you are looking for?",
+    },
+    {
+      checklistQuery: "Looking for a specific research?",
+    },
+    {
+      checklistQuery: "Request a feature?",
+    },
+    {
+      checklistQuery: "Want to share your experience?",
+    },
+    {
+      checklistQuery: "Want to report a bug?",
+    },
+  ];
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevState) => ({
@@ -34,181 +54,159 @@ const Contact = () => {
         <h3 className="h3">Connect with Us</h3>
 
         <h1 className="h1">Contact Us</h1>
-        <h3 className="heading3">The VC Project is your one stop destination to read about researches around the globe 
-                        share <br />
-                        <br /> 
-                        your experience with us and help us improve our platform, we would love to hear from you ...
+        <h3 className="heading3">
+          The VC Project is your one stop destination to read about researches
+          around the globe share <br />
+          <br />
+          your experience with us and help us improve our platform, we would
+          love to hear from you ...
         </h3>
       </div>
 
-     <div className="bothcontainers">
-     <div className="container-left">
-        <p className="paragraph">
-          "One of the standout features of the VC Project Repository is its
-          user-friendly interface. Navigating through the platform was
-          intuitive, allowing our team to effortlessly browse through a diverse
-          range of research topics. The search functionality was robust,
-          enabling us to quickly locate specific studies or explore broader
-          themes within the VC domain. Additionally, the categorization of
-          researches based on relevant tags enhanced the overall user
-          experience, ensuring that users could easily find content tailored to
-          their interests."
-          <br />
-          <br />
-          Shashank Mishra
-          <br />
-          <h4 className="h4">CTO</h4>
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque,
-            asperiores, incidunt recusandae, ducimus possimus obcaecati sequi
-            veniam laborum repudiandae quam odio commodi minus sint! Fugiat fuga
-            magni deserunt iure quod. Lorem ipsum dolor sit amet consectetur
+      <div className="bothcontainers">
+        <div className="container-left">
+          <p className="paragraph">
+            "One of the standout features of the VC Project Repository is its
+            user-friendly interface. Navigating through the platform was
+            intuitive, allowing our team to effortlessly browse through a
+            diverse range of research topics. The search functionality was
+            robust, enabling us to quickly locate specific studies or explore
+            broader themes within the VC domain. Additionally, the
+            categorization of researches based on relevant tags enhanced the
+            overall user experience, ensuring that users could easily find
+            content tailored to their interests."
+            <br />
+            <br />
+            Shashank Mishra
+            <br />
+            <h4 className="h4">CTO</h4>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque,
+              asperiores, incidunt recusandae, ducimus possimus obcaecati sequi
+              veniam laborum repudiandae quam odio commodi minus sint! Fugiat
+              fuga magni deserunt iure quod. Lorem ipsum dolor sit amet
+              consectetur
+            </p>
           </p>
-        </p>
-      </div>
-      <div className="container-right">
-        <div className="container-right-small">
-          <form className="form" onSubmit={handleSubmit}>
-            <div className="nameinput">
-              <div className="name">
-                <label className="namechange" htmlFor="name">
-                  First Name
+        </div>
+        <div className="container-right">
+          <div className="container-right-small">
+            <form className="form" onSubmit={handleSubmit}>
+              <div className="nameinput">
+                <div className="name">
+                  <label className="namechange" htmlFor="name">
+                    First Name
+                  </label>
+                  <input
+                    className="inputname"
+                    type="text"
+                    id="name"
+                    name="name"
+                    value={formData.name}
+                    placeholder="First Name"
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+
+                <div className="name">
+                  <label className="namechange" htmlFor="name">
+                    Last Name
+                  </label>
+                  <input
+                    className="inputname"
+                    type="text"
+                    id="name"
+                    name="name"
+                    placeholder="Last Name"
+                    value={formData.name}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+              </div>
+
+              <div className="email">
+                <label className="mail" htmlFor="email">
+                  Email
                 </label>
                 <input
-                  className="inputname"
-                  type="text"
-                  id="name"
-                  name="name"
-                  value={formData.name}
-                  placeholder="First Name"
+                  className="inputmail"
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="Email Address"
+                  value={formData.email}
                   onChange={handleChange}
                   required
                 />
               </div>
-
-              <div className="name">
-                <label className="namechange" htmlFor="name">
-                  Last Name
-                </label>
-                <input
-                  className="inputname"
-                  type="text"
-                  id="name"
-                  name="name"
-                  placeholder="Last Name"
-                  value={formData.name}
-                  onChange={handleChange}
-                  required
-                />
+              <button className="uploadbutton" type="upload">
+                Upload Files
+              </button>
+              <div className="checklistBox">
+                {checklistData.map(({checklistQuery})=>(
+                  <label className="checklistItems" htmlFor="checklist">
+                    <input
+                      type="checkbox"
+                      id="checklist"
+                      name="checklist"
+                      value={checklistQuery}
+                      className="checklistInput"
+                    />
+                    {checklistQuery}
+                  </label>
+                ))}
               </div>
-            </div>
 
-            <div className="email">
-              <label className="mail" htmlFor="email">
-                Email
+              <label className="messg" htmlFor="message">
+                Tell us about your experience here
               </label>
-              <input
-                className="inputmail"
-                type="email"
-                id="email"
-                name="email"
-                placeholder="Email Address"
-                value={formData.email}
-                onChange={handleChange}
-                required
-              />
-             
-            </div>
-            <button className="uploadbutton" type="upload">Upload Files</button>
-            <div className="checklist">
-              <div className="check">
-                <input type="checkbox" id="checklist" name="checklist" />
-                <label className="listcheck" htmlFor="checklist">
-                  Problem in uploading research?
-                </label>
+              <div className="textarea">
+                {" "}
+                <textarea
+                  className="textarea"
+                  id="message"
+                  name="message"
+                  placeholder="Write your message here."
+                  value={formData.message}
+                  onChange={handleChange}
+                  rows="5"
+                  required
+                ></textarea>
               </div>
 
-              <div className="check">
-                <input type="checkbox" id="checklist2" name="checklist2" />
-                <label className="listcheck" htmlFor="checklist2">
-                  Unable to find the research you are looking for?
-                </label>
-              </div>
-              <div className="check">
-                <input type="checkbox" id="checklist2" name="checklist2" />
-                <label className="listcheck" htmlFor="checklist2">
-                  Looking for a specific research?
-                </label>
-              </div>
-              <div className="check">
-                <input type="checkbox" id="checklist2" name="checklist2" />
-                <label className="listcheck" htmlFor="checklist2">
-                  Request a feature?
-                </label>
-              </div>
-              <div className="check">
-                <input type="checkbox" id="checklist2" name="checklist2" />
-                <label className="listcheck" htmlFor="checklist2">
-                  Want to share your experience?
-                </label>
-              </div>
-              <div className="check">
-                <input type="checkbox" id="checklist2" name="checklist2" />
-                <label className="listcheck" htmlFor="checklist2">
-                  Want to report a bug?
-                </label>
-              </div>
-            </div>
-
-            <label className="messg" htmlFor="message">
-              Tell us about your experience here
-            </label>
-            <div className="textarea">
-              {" "}
-              <textarea
-                className="textarea"
-                id="message"
-                name="message"
-                placeholder="Write your message here."
-                value={formData.message}
-                onChange={handleChange}
-                rows="5"
-                required
-              ></textarea>
-            </div>
-
-            <button className="submitbutton" type="submit">
-              Submit
-            </button>
-          </form>
+              <button className="submitbutton" type="submit">
+                Submit
+              </button>
+            </form>
+          </div>
         </div>
       </div>
-     </div>
-     <Accordion/>
+      <Accordion />
     </div>
-    
   );
 };
 export default Contact;
