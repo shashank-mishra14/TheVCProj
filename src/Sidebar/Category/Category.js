70ad20c578f88a55057687aa3af3b4b2239ebaec
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-02-25 19:45:48 +0530

    added categories data

--- src/Sidebar/Category/Category.js
+++ src/Sidebar/Category/Category.js
@@ -1,6 +1,81 @@
 import "./Category.css";
 import Input from "../../components/Input";
+import React, { useEffect, useState } from "react";
 
+
+
+const categoriesData = [
+  {
+    category: "Agritech",
+    subcategories: ["Agritech"],
+  },
+  {
+    category: "Climate & Sustainability",
+    subcategories: ["Circular Economy"],
+  },
+  {
+    category: "Consumer Tech",
+    subcategories: [
+      "Gaming",
+      "Marketplaces",
+      "Edtech",
+      "Indian Consumer",
+      "Beauty & Personal care",
+      "Ecommerce",
+      "Foodtech",
+      "B2B",
+      "D2C",
+    ],
+  },
+  {
+    category: "Content & Media",
+    subcategories: ["Creator Economy"],
+  },
+  {
+    category: "Employment",
+    subcategories: ["Blue Collar Workforce"],
+  },
+  {
+    category: "EV & Renewables",
+    subcategories: ["BatteryTech", "EV"],
+  },
+  {
+    category: "Financial Services",
+    subcategories: ["Fintech", "SME Credit"],
+  },
+  {
+    category: "Healthcare",
+    subcategories: ["Mental Health", "Chronic Care"],
+  },
+  {
+    category: "Human Resources",
+    subcategories: ["Talent Trends"],
+  },
+  {
+    category: "India Stack",
+    subcategories: ["DPI"],
+  },
+  {
+    category: "Infrastructure & Security",
+    subcategories: ["Dev tools"],
+  },
+  {
+    category: "Insurance",
+    subcategories: ["Insurtech"],
+  },
+  {
+    category: "Social & Advertising",
+    subcategories: ["Adtech", "Social Media", "Short form video (SFV)"],
+  },
+  {
+    category: "Software",
+    subcategories: ["Cybersecurity", "HRTech", "Indian SaaS"],
+  },
+  {
+    category: "Venture Capital & Private Equity",
+    subcategories: ["Funding Trends", "VC Returns", "Termsheets"],
+  },
+];
 function Category({ handleChange }) {
   return (
     <div>
