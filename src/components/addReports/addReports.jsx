3776e719301201329b1d351dcb0a3c8a501acd1d
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-02-26 11:39:49 +0530

    down with the form

--- src/components/addReports/addReports.jsx
+++ src/components/addReports/addReports.jsx
@@ -31,89 +31,92 @@ const useAddReports = () => {
     });
   };
 
-  return <div className="reportContainer">
-    <form action="" className="submitreport" onSubmit={handleSubmit}>
-    <div className="Categoryinput">
-                <div className="category">
-                  <label className="categorychange" htmlFor="name">
-                    Category
-                  </label>
-                  <input
-                    className="inputCategory"
-                    type="text"
-                    id="category"
-                    name="category"
-                    value={reportData.category}
-                    placeholder="Category"
-                    onChange={handleChange}
-                    required
-                  />
-                </div>
+  return (
+    <div className="reportContainer">
+      <form action="" className="submitreport" onSubmit={handleSubmit}>
+        <div className="Categoryinput">
+          <div className="category">
+            <label className="categorychange" htmlFor="name">
+              Category
+            </label>
+            <input
+              className="inputCategory"
+              type="text"
+              id="category"
+              name="category"
+              value={reportData.category}
+              placeholder="Category"
+              onChange={handleChange}
+              required
+            />
+          </div>
 
-                <div className="subcategory">
-                  <label className="subcategorychange" htmlFor="name">
-                  Subcategory
-                  </label>
-                  <input
-                    className="inputSubcategory"
-                    type="text"
-                    id="subcategory"
-                    name="Subcategory"
-                    placeholder="Subcategory"
-                    value={reportData.subcategory}
-                    onChange={handleChange}
-                    required
-                  />
-                </div>
-              </div>
+          <div className="subcategory">
+            <label className="subcategorychange" htmlFor="name">
+              Subcategory
+            </label>
+            <input
+              className="inputSubcategory"
+              type="text"
+              id="subcategory"
+              name="Subcategory"
+              placeholder="Subcategory"
+              value={reportData.subcategory}
+              onChange={handleChange}
+              required
+            />
+          </div>
+        </div>
 
-              <div className="author">
-                <label className="authorinput" htmlFor="email">
-                  Author
-                </label>
-                <input
-                  className="inputauthor"
-                  type="text"
-                  id="author"
-                  name="author"
-                  placeholder="Author"
-                  value={reportData.author}
-                  onChange={handleChange}
-                  required
-                />
-              </div>
-              <div className="year">
-                <label className="yearchange" htmlFor="year">
-                  Year
-                </label>
-                <input
-                  className="inputyear"
-                  type="year"
-                  id="year"
-                  name="year"
-                  placeholder="Year"
-                  value={reportData.year}
-                  onChange={handleChange}
-                  required
-                />
-              </div>
-              <div className="imgsrc">
-                <label className="imgsrcChange" htmlFor="email">
-                  
-                </label>
-                <input
-                  className="inputimgsrc"
-                  type="text"
-                  id="imgsrc"
-                  name="imgsrc"
-                  placeholder="Add your thubmnail image link here"
-                  value={reportData.imgsrc}
-                  onChange={handleChange}
-                  required
-                />
-              </div>
-    </form>
-  </div>;
+        <div className="author">
+          <label className="authorinput" htmlFor="email">
+            Author
+          </label>
+          <input
+            className="inputauthor"
+            type="text"
+            id="author"
+            name="author"
+            placeholder="Author"
+            value={reportData.author}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="year">
+          <label className="yearchange" htmlFor="year">
+            Year
+          </label>
+          <input
+            className="inputyear"
+            type="year"
+            id="year"
+            name="year"
+            placeholder="Year"
+            value={reportData.year}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="imgsrc">
+          <label className="imgsrcChange" htmlFor="email"></label>
+          <input
+            className="inputimgsrc"
+            type="text"
+            id="imgsrc"
+            name="imgsrc"
+            placeholder="Add your thubmnail image link here"
+            value={reportData.imgsrc}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button className="submitbutton" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default useAddReports;
