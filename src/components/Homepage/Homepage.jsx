e30e23918326b3c66f1b7b770350267e744e4213
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-03-01 17:56:50 +0530

    changes in ui

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -4,16 +4,21 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const Homepage = () => {
   return (
-    <div className="homepage-main">
-        <div className="homepage-logo"></div>
-      Homepage
-      <div className="homepage-navbar">
-        <div>Reports</div>
-        <div>About Us</div>
-        <div>Contact Us</div>
-        <div>Pricing</div>
+   <div className="main">
+     <div className="homepage-main">
+      <div className="homepage-navbar-container">
+        <div className="homepage-logo">
+          <h3>The VC Project</h3>
+        </div>
+        <div className="homepage-navbar">
+          <div>Reports</div>
+          <div>About Us</div>
+          <div>Contact Us</div>
+          <div>Pricing</div>
+        </div>
       </div>
     </div>
+   </div>
   );
 };
 
