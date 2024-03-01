b6cf8030491af60c60905e9d8ca4a080d6c55081
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-03-01 16:33:45 +0530

    new changes

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-import "./Homepage.css"
+import React from "react";
+import "./Homepage.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 const Homepage = () => {
   return (
-    <div>Homepage</div>
-  )
-}
+    <div className="homepage-main">
+        <div className="homepage-logo"></div>
+      Homepage
+      <div className="homepage-navbar">
+        <div>Reports</div>
+        <div>About Us</div>
+        <div>Contact Us</div>
+        <div>Pricing</div>
+      </div>
+    </div>
+  );
+};
 
-export default Homepage
+export default Homepage;
