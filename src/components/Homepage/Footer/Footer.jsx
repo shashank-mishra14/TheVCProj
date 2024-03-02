90670afd8d4c1853598d5dee602b74de62a98cfc
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-03-02 20:56:51 +0530

    adding ui in footer

--- src/components/Homepage/Footer/Footer.jsx
+++ src/components/Homepage/Footer/Footer.jsx
@@ -1,44 +1,74 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import "./Footer.css";
 const Footer = () => {
   return (
     <div>
-    <footer class="footer">
-         <div class="container">
-             <div class="row">
-                 <div class="footer-col">
-                     <h4>The VC Project</h4>
-                     <ul>
-                         <li><a href="#">about us</a></li>
-                         <li><a href="#">our services</a></li>
-                         <li><a href="#">privacy policy</a></li>
-                         
-                     </ul>
-                 </div>
-                 <div class="footer-col">
-                     <h4>get help</h4>
-                     <ul>
-                         <li><a href="#">FAQ</a></li>
-                         <li><a href="#">shipping</a></li>
-                         <li><a href="#">returns</a></li>
-                         <li><a href="#">order status</a></li>
-                         <li><a href="#"></a></li>
-                     </ul>
-                 </div>
-                
-                 <div class="footer-col">
-                     <h4>follow us</h4>
-                     <div class="social-links">
-                         <a href="#"><i class="fab fa-facebook-f"></i></a>
-                         <a href="#"><i class="fab fa-twitter"></i></a>
-                         <a href="#"><i class="fab fa-instagram"></i></a>
-                         <a href="#"><i class="fab fa-linkedin-in"></i></a>
-                     </div>
-                 </div>
-             </div>
-         </div>
-    </footer></div>
-  )
-}
+      <footer class="footer">
+        <div class="container">
+          <div class="row">
+            <div className="tag">
+            <div className="main-tag">The VC Project</div>
+            <p>
+              India's largest research repository <br /> for students
+            </p>
+            </div>
+            <div class="footer-col">
+              <h4>Company</h4>
+              <ul>
+                <li>
+                  <a href="#">about us</a>
+                </li>
+                <li>
+                  <a href="#">our services</a>
+                </li>
+                <li>
+                  <a href="#">privacy policy</a>
+                </li>
+              </ul>
+            </div>
+            <div class="footer-col">
+              <h4>get help</h4>
+              <ul>
+                <li>
+                  <a href="#">FAQ</a>
+                </li>
+                <li>
+                  <a href="#">Cookies Policies</a>
+                </li>
+                <li>
+                  <a href="#">Privacy Policies</a>
+                </li>
+                <li>
+                  <a href="#">Data Processing</a>
+                </li>
+                <li>
+                  <a href="#"></a>
+                </li>
+              </ul>
+            </div>
 
-export default Footer
+            <div class="footer-col">
+              <h4>follow us</h4>
+              <div class="social-links">
+                <a href="#">
+                  <i class="fab fa-facebook-f"></i>
+                </a>
+                <a href="#">
+                  <i class="fab fa-twitter"></i>
+                </a>
+                <a href="#">
+                  <i class="fab fa-instagram"></i>
+                </a>
+                <a href="#">
+                  <i class="fab fa-linkedin-in"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
