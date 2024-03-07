011b0100c7f3aa928169ef55a4d0d4676c52426a
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-03-08 00:16:11 +0530

    changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,41 +1,64 @@
 import "./Card.css";
+import React from "react";
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-const Card = ({ img, title, companyName, subcategory, year,category,month,link }) => {
-    const MAX_TITLE_LENGTH = 60;
-  
-    let displayedTitle = title;
-    if (title.length > MAX_TITLE_LENGTH) {
+const Card = ({ img, title, companyName, subcategory, year, category, month, link }) => {
+  const MAX_TITLE_LENGTH = 60;
+  let displayedTitle = title;
+
+  // Check if the length of the title is less than 5 characters
+  if (title.length < 5) {
+      displayedTitle = (
+          <React.Fragment>
+              {title}
+              <br />
+          </React.Fragment>
+      );
+  }
+
+  if (title.length > MAX_TITLE_LENGTH) {
       displayedTitle = title.substring(0, MAX_TITLE_LENGTH) + "...";
-    }
-    
-    const handleReadMoreClick = () => {
-      window.open(link, '_blank');; // Redirect to the link stored in your database
+  }
+
+  const MAX_CATEGORY_LENGTH = 15;
+  let displayedCategory = subcategory;
+  if (subcategory.length > MAX_CATEGORY_LENGTH) {
+      // Find the index of the last space within the maximum length
+      let lastSpaceIndex = MAX_CATEGORY_LENGTH;
+      for (let i = MAX_CATEGORY_LENGTH - 1; i >= 0; i--) {
+          if (subcategory[i] === " ") {
+              lastSpaceIndex = i;
+              break;
+          }
+      }
+
+      // Truncate at the last space index or simply at the maximum length
+      displayedCategory = subcategory.substring(0, lastSpaceIndex) + "...";
+  }
+
+  const handleReadMoreClick = () => {
+      window.open(link, '_blank'); // Redirect to the link stored in your database
   };
 
-    return (
+  return (
       <>
-        <section className="card" onClick={
-              handleReadMoreClick
-            }>
-          <div className="square">
-          <img src={img} alt={title} className="card-img" />
-          <div className="card-details">
-            <h3 className="card-title">{displayedTitle}</h3>
-            <div className="card-company">by {companyName}</div>
-            <button className="tag-button">{category}</button>
-            <button className="tag-button">{subcategory}</button>
-            <button className="tag-button">{year}</button>
-            <button className="tag-button">{month}</button>
-            <hr className="horizontal-line" />
-            <button className="readmore-button" >Read More</button>
-
-          </div>
-          </div>
-          
-        </section>
+          <section className="card" onClick={handleReadMoreClick}>
+              <div className="square">
+                  <img src={img} alt={title} className="card-img" />
+                  <div className="card-details">
+                      <h3 className="card-title">{displayedTitle}</h3>
+                      <div className="card-company">by {companyName}</div>
+                      <button className="tag-button">{category}</button>
+                      <button className="tag-button">{displayedCategory}</button>
+                      <button className="tag-button">{year}</button>
+                      <button className="tag-button">{month}</button>
+                      <hr className="horizontal-line" />
+                      <button className="readmore-button">Read More</button>
+                  </div>
+              </div>
+          </section>
       </>
-    );
-  };
-  
-  export default Card;
+  );
+};
+
+export default Card;
